2a3afe05bd058e9edb1538e56d2b983bd1e04175
DanielMCL <79699178+DanielMCL@users.noreply.github.com>
2021-05-15 19:58:24 +0200

    Hecho el 6.2, creo que entero pero no puedo asegurarlo

--- Workspace-Practica2/Practica2/src/simulator/view/BodiesTable.java
+++ Workspace-Practica2/Practica2/src/simulator/view/BodiesTable.java
@@ -5,17 +5,26 @@ import java.awt.Color;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.TitledBorder;
 
 import simulator.control.Controller;
 
 public class BodiesTable extends JPanel {
+	private static final long serialVersionUID = 1L;
+
 	public BodiesTable(Controller ctrl) {
 		setLayout(new BorderLayout());
 		setBorder(BorderFactory.createTitledBorder(
 		BorderFactory.createLineBorder(Color.black, 2),
 		"Bodies",
 		TitledBorder.LEFT, TitledBorder.TOP));
-		// TODO complete
+
+		BodiesTableModel tableModel = new BodiesTableModel(ctrl);
+		JTable table = new JTable(tableModel);
+		JScrollPane scrollPane = new JScrollPane(table);
+		
+		this.add(scrollPane, BorderLayout.CENTER);
 	}
 }
--- Workspace-Practica2/Practica2/src/simulator/view/BodiesTableModel.java
+++ Workspace-Practica2/Practica2/src/simulator/view/BodiesTableModel.java
@@ -10,6 +10,8 @@ import simulator.model.Body;
 import simulator.model.SimulatorObserver;
 
 public class BodiesTableModel extends AbstractTableModel implements SimulatorObserver {
+	private static final long serialVersionUID = 1L;
+	
 	private List<Body> _bodies;
 	
 	public BodiesTableModel(Controller ctrl) {
@@ -17,64 +19,83 @@ public class BodiesTableModel extends AbstractTableModel implements SimulatorObs
 		ctrl.addObserver(this);
 	}
 
-
 	@Override
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 5;
 	}
 
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return _bodies.size();
 	}
 	
 	@Override
 	public String getColumnName(int column) {
-		return "Dani puto niño";
+		switch(column) {
+		case 0:
+			return "Id";
+		case 1:
+			return "Mass";
+		case 2:
+			return "Position";
+		case 3:
+			return "Velocity";
+		case 4:
+			return "Force";
+		}
+		return null;
 	}
 
 	@Override
 	public Object getValueAt(int arg0, int arg1) {
-		// TODO Auto-generated method stub
+		switch(arg1) {
+		case 0:
+			return _bodies.get(arg0).getId();
+		case 1:
+			return _bodies.get(arg0).getMass();
+		case 2:
+			return _bodies.get(arg0).getPosition();
+		case 3:
+			return _bodies.get(arg0).getVelocity();
+		case 4:
+			return _bodies.get(arg0).getForce();
+		}
 		return null;
 	}
 
 	@Override
 	public void onRegister(List<Body> bodies, double time, double dt, String fLawsDesc) {
-		// TODO Auto-generated method stub
-		
+		_bodies = bodies;
+		fireTableStructureChanged();
 	}
 
 	@Override
 	public void onReset(List<Body> bodies, double time, double dt, String fLawsDesc) {
-		// TODO Auto-generated method stub
-		
+		_bodies = bodies;
+		fireTableStructureChanged();
 	}
 
 	@Override
 	public void onBodyAdded(List<Body> bodies, Body b) {
-		// TODO Auto-generated method stub
-		
+		_bodies = bodies;
+		fireTableStructureChanged();
 	}
 
 	@Override
 	public void onAdvance(List<Body> bodies, double time) {
-		// TODO Auto-generated method stub
-		
+		_bodies = bodies;
+		fireTableStructureChanged();
 	}
 
 	@Override
 	public void onDeltaTimeChanged(double dt) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void onForceLawsChanged(String fLawsDesc) {
 		// TODO Auto-generated method stub
-		
 	}
+	
 	
 }
